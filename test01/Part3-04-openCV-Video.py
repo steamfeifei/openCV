84907417ee72326ab75c729959dc76fe46e3d95e
gaofei <1010155688@qq.com>
2019-01-24 21:15:05 +0800

    初始化

--- test01/Part3-04-openCV-Video.py
+++ test01/Part3-04-openCV-Video.py
@@ -1,31 +1,20 @@
 import numpy as np
-# import cv2
-#
-# cap = cv2.VideoCapture(0)
-#
-# while True:
-#     ret, frame = cap.read()     # 第一个参数为True/False,是否读到图片，第二个参数为截道德每一帧德图片
-#     # print(ret, frame)
-#     print(cap.get(3), cap.get(4))
-#
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#
-#     cv2.imshow('frame', gray)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-#
-# cap.release()
-# cv2.destroyAllWindows()
-#
-#
-# cap = cv2.VideoCapture(0)
-# while True:
-#     ret, frame = cap.read()
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR)
-#     cv2.imshow('frame', gray)
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
-import numpy as np
 import cv2
 
+cap = cv2.VideoCapture(0)
+
+while(True):
+ # Capture frame-by-frame
+ret, frame = cap.read()
+
+# Our operations on the frame come here
+gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+# Display the resulting frame
+cv2.imshow('frame',gray)
+if cv2.waitKey(1) & 0xFF == ord('q'):
+    break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
