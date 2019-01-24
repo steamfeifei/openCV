c07de7253e1e9027bd27b59d6d77303ba0b6d517
gaofei <1010155688@qq.com>
2019-01-24 20:56:30 +0800

    初始化

---
+++ test01/Part3-04-openCV-Video.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()     # 第一个参数为True/False,是否读到图片，第二个参数为截道德每一帧德图片
+    # print(ret, frame)
+    print(cap.get(3),cap.get(4))
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('frame', gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
